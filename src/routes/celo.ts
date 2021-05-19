ee412708b2c949a29a1500820a419af60f07e33f
Girish Nuli <girish@girishnuli.com>
2021-05-19 14:24:08 +0530

    celo discover index

--- src/routes/celo.ts
+++ src/routes/celo.ts
@@ -2,8 +2,8 @@ import CeloReviewSend from '../pages/celo/transact/CeloReviewSend.svelte'
 import CeloSendAddress from '../pages/celo/transact/CeloSendAddress.svelte'
 import CeloIndex from '../pages/celo/CeloIndex.svelte'
 import CeloDiscoverIndex from '../pages/celo/actions/discover/CeloDiscoverIndex.svelte'
-import CeloEarnIndex from '../pages/celo/actions/discover/CeloEarnIndex.svelte'
-import CeloVoteIndex from '../pages/celo/actions/discover/CeloVoteIndex.svelte'
+import CeloEarnIndex from '../pages/celo/actions/earn/CeloEarnIndex.svelte'
+import CeloVoteIndex from '../pages/celo/actions/vote/CeloVoteIndex.svelte'
 import { navRoutes } from '../constants/navRoutes'
 
 export default {
